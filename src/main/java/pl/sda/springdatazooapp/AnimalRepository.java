e2b129f0a39625eec8bca20350576c411da49b8e
DESKTOP-95T3DT5\Michał Mielczarek <michal.mielczarek@gmail.com>
2018-09-26 06:18:43 +0200

    query in repository

--- src/main/java/pl/sda/springdatazooapp/AnimalRepository.java
+++ src/main/java/pl/sda/springdatazooapp/AnimalRepository.java
@@ -2,6 +2,7 @@ package pl.sda.springdatazooapp;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface AnimalRepository extends MongoRepository<Animal, String> {
@@ -13,5 +14,9 @@ public interface AnimalRepository extends MongoRepository<Animal, String> {
     List<Animal> findAllByWeightAndHeightAndLegsBetween(Integer weight, Integer height, Integer legsMin, Integer legsMax);
 
     List<Animal> findAllByNameContainingAndCountry (String name, String country);
+
+    List<Animal> findAllByBirthBetween (LocalDate birthMin, LocalDate birthMax);
+
+    List<Animal> findAllByNameInAndCountryIn (String name, String country)
 
 }
